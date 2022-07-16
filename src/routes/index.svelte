d1802169ea3d35ab3121195cc2eb555de2874c36
PY44N <pyan4444@gmail.com>
2022-07-16 10:57:15 -0400

    Shoved some random text onto the screen (someone besides me should probably design the site)

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -6,5 +6,13 @@
 </svelte:head>
 
 <main>
-	<h1>Hello World</h1>
+	<h1 class="text-2xl text-center pt-2">Cold Spring Harbor Cyberhawks</h1>
+	<p class="p-5">
+		The Cold Spring Harbor Cyberhawks Robotics Team is made up of students in grades 9-12 at Cold
+		Spring Harbor High School where we design, build and program our robots. We are part of the
+		international “For Inspiration and Recognition of Science and Technology” (FIRST) Robotics
+		program. Our team participates in several annual robotics competitions. By teaching our members
+		life skills, ranging from teamwork to project management, robotics sets up students for success
+		in more than just engineering.
+	</p>
 </main>
